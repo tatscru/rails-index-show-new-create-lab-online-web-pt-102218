916791b22a288eda5b25e8eb7d3c1764823cfbdb
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-17 00:14:11 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -14,6 +14,6 @@ class CouponsController < ApplicationController
   def create
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
     
-    redirect_to
+    redirect_to coupon_path(@coupon)
   end 
 end 
