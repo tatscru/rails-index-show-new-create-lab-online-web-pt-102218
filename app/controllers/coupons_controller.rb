23a568bdb46e3b97999f8add87e62001cedeb55f
tatiana.mojica <tatiana.mojica@bustle.com>
2019-04-17 00:12:00 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,6 +12,6 @@ class CouponsController < ApplicationController
   end 
   
   def create
-    @coupon = Coupon.create()
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
   end 
 end 
