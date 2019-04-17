5e12ef577f02415ef8e83668d78bbe726e08eaa8
github username <github email address>
2019-04-17 13:21:17 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,14 +6,14 @@ class CouponsController < ApplicationController
   
   def show 
     @coupon = Coupon.find(params[:id])
+    puts params[:id]
   end 
   
   def new 
   end 
   
   def create
-    @coupon = Coupon.create(store: params[:store], coupon_code: params[:coupon_code])
-    
+    @coupon = Coupon.create(store: params[:coupon][:store], coupon_code: params[:coupon][:coupon_code])
     redirect_to coupon_path(@coupon)
     # here we are redirecting to the new resources show page
   end 
